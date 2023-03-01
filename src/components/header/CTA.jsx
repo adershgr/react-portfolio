031fd699f3723d69d52f80d5c1af622e67f26aba
adershgr <mail4me.adersh@gail.com>
2023-03-01 11:22:01 +0530

    resume updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CV from '../../assets/CV.pdf';
+import AdershGR from '../../assets/AdershGR.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={AdershGR} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
