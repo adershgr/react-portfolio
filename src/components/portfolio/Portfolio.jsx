2f698d364c61e985bf9668dd29d0f5ae16ee6573
adershgr <mail4me.adersh@gail.com>
2023-03-28 02:14:58 +0530

    spelling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -76,7 +76,7 @@ const Portfolio = () => {
 
   return (
     <section id="portfolio">
-      <h5>My Recent Work</h5>
+      <h5>My Recent Works</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
